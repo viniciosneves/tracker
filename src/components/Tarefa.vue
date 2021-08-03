68022dd46bbaa094c076b669dff05c9c11eba4b1
Vinicios Neves <vinicios@gipsydanger.local>
2021-08-03 18:10:34 -0300

    incluindo slots no escopo

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <Box>
     <div class="columns">
       <div class="column is-7">
         {{ tarefa.descricao || 'Tarefa sem descrição' }}
@@ -8,18 +8,20 @@
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
       </div>
     </div>
-  </div>
+  </Box>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import Cronometro from "./Cronometro.vue";
+import Box from "./Box.vue";
 import ITarefa from "../interfaces/ITarefa";
 
 export default defineComponent({
   name: 'Tarefa',
   components: {
     Cronometro,
+    Box
   },
   props: {
     tarefa: {
@@ -36,11 +38,3 @@ export default defineComponent({
   }
 });
 </script>
-<style scoped>
-.box {
-  background: #FAF0CA;
-}
-.descricao {
-  font-weight: bold;
-}
-</style>
